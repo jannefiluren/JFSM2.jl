40cc7a59888a7958707bbdf889cf05c2cb7fbd30
jmgnve <jmgnve@nve.no>
2018-07-10 10:33:47 +0200

    compil script

--- src/JFSM2.jl
+++ src/JFSM2.jl
@@ -1,5 +1,12 @@
 module JFSM2
 
-# package code goes here
+using DataFrames
 
-end # module
+# Functions for compilation
+
+export write_modcfg, compile_fsm, cfg_table, compile_all
+
+include("compil.jl")
+
+
+end
